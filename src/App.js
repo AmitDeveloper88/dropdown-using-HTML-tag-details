073144142f841305f727520b563580ef5616cfae
AmitDeveloper88 <104545251+AmitDeveloper88@users.noreply.github.com>
2022-05-20 11:20:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>dropdown with only HTML tag(detail tag)</h1>
+      <details>
+        <summary>Menu 1</summary>
+        <p>go to menu1</p>
+      </details>
+    </div>
+  );
+}
